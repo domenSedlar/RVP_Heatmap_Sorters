4d177822120198d577efa2b156d1044bb52e07fb
domenSedlar <domen.sedlar@gmail.com>
2026-08-22 17:20:41 +0200

    created a program to evaluate algorithms on a large corpus of matrices

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_size(filepath):
     return A.shape
 
 def get_metadata(filenm, dirpath, algo_nm, tm, dataset, metric):
-    (n,m) = get_size(os.join(dirpath, filenm))
+    (n,m) = get_size(os.path.join(dirpath, filenm))
     row = {
         'file_name': filenm,
         'dir' : dirpath,
@@ -55,19 +55,35 @@ def get_metadata(filenm, dirpath, algo_nm, tm, dataset, metric):
     return row
 
 
-def run(algo, in_dir, algo_nm, tm, dataset, metric, output_path='results.parquet', only_small=True):
+def run(algo, in_dir, algo_nm, dataset, metric, output_path='results.parquet', only_small=True):
     if only_small:
-        in_dir = os.join(in_dir, 'Small')
-
+        in_dir = os.path.join(in_dir, 'Small')
+    tm = 0
     df = []
     for root, dirs, files in os.walk(in_dir):
         for f in files:
             if '.tsv' in f:
-                res = ev(os.join(root, f), algo)
-                row = get_metadata(f, root, algo_nm, tm, dataset, metric) + res
+                start_time = time.perf_counter()
+                res = ev(os.path.join(root, f), algo)
+                end_time = time.perf_counter()
+                tm = end_time - start_time
+
+                row = get_metadata(f, root, algo_nm, tm, dataset, metric) | res
                 df.append(row)
 
     df = pd.DataFrame(df)
+    
     df.to_parquet(
-    output_path, engine="fastparquet", append=True, index=False
-)
+    output_path, engine="fastparquet", append=os.path.exists(output_path), index=False
+        )
+
+if __name__ == "__main__":
+    run(
+        algo = bae,
+        in_dir = 'Data',
+        algo_nm = 'BAE',
+        dataset='test',
+        metric='NS',
+        output_path='test_results.parquet',
+        only_small=True
+    )
